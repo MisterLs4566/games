e677396b2500b58477bdb80a66ccbcb5ba98c481
MisterLs4566 <lkronenb1@googlemail.com>
2021-09-07 00:08:30 +0200

    version 4.1

--- Robot/script.js
+++ Robot/script.js
@@ -10,8 +10,9 @@ class Game{
         this.c.imageSmoothingEnabled = false;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
-        this.player = new Player("sprites/static.png", 10, this.height-(parseInt(this.height/2.2)), parseInt(this.height/3));
-        this.ground = new Ground("sprites/ground1.PNG", 0, this.height-parseInt(this.width/2.5), this.width, parseInt(this.width/2), 1);
+        this.ground = new Ground("sprites/ground1.PNG", 0, this.height-parseInt(this.width/2.19), this.width, parseInt(this.width/2), 1);
+        this.ground.ground = new Ground("sprites/ground1.PNG", this.ground.pos_x+this.ground.scalex, this.ground.pos_y, this.ground.scalex, this.ground.scaley, 2)
+        this.player = new Player("sprites/static.png", 10, this.height-(parseInt(this.height/1.75)), parseInt(this.height/3));
     }
     clear(){
         this.c.clearRect(0, 0, this.width, this.height);
@@ -34,13 +35,15 @@ class Game{
     }
     draw(event){
         this.ground.image.onload = this.c.drawImage(this.ground.image, this.ground.x, this.ground.y, this.ground.scalex, this.ground.scaley);
+        this.ground.ground.image.onload = this.c.drawImage(this.ground.image, this.ground.ground.x, this.ground.ground.y, this.ground.ground.scalex, this.ground.ground.scaley);
         this.player.image.onload = this.c.drawImage(this.player.image, this.player.x, this.player.y, this.player.scale, this.player.scale);
     }
     update(){
         this.clear();
         this.draw();
         this.player.update();
-        this.ground.update()
+        this.ground.update();
+        this.ground.ground.update();
     }
 }
 class Player{
@@ -61,7 +64,7 @@ class Player{
     }
     gravity(){
         if (this.y < this.pos_y){
-            this.y += 4;
+            this.y += 7;
         }
     }
     update(){
@@ -83,8 +86,26 @@ class Ground{
     update(){
         if (this.type == 1)
         {
-            this.x --;
+            this.ground.x = this.x+this.scalex-20
+            this.x -= 10;
         }
+        if (this.type == 2)
+        {
+            if (this.x <= 0)
+            {
+                game.ground.x = this.x
+            }
+        }
+    }
+}
+class Spike{
+    constructor(image, pos_x, pos_y, scalex, scaley){
+        this.image = new Image();
+        this.image.src = image;
+        this.pos_x = pos_x
+        this.pos_y = pos_y
+        this.scalex = scalex
+        this.scaley = scaley
     }
 }
 game = new Game();
